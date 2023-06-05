6e307128b9a8d3f7c54486e7e036e446ecb095be
Daniel Loomb <daniel@statsig.com>
2023-06-05 10:56:30 -0700

    build

--- dist/index.js
+++ dist/index.js
@@ -1,64 +1,6 @@
 require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 5008:
-/***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.createGitRepoUrl = exports.validateAndExtractArgsFromPayload = void 0;
-const types_1 = __nccwpck_require__(8164);
-const PRIV_TO_PUB_REPO_MAP = {
-    'private-js-client-sdk': 'js-client',
-    'private-rust-sdk': 'rust-sdk',
-    'test-sdk-repo-private': 'test-sdk-repo-public'
-};
-function validateAndExtractArgsFromPayload(payload) {
-    var _a, _b, _c, _d, _e;
-    const ref = (_b = (_a = payload.pull_request) === null || _a === void 0 ? void 0 : _a.head) === null || _b === void 0 ? void 0 : _b.ref;
-    const sha = (_d = (_c = payload.pull_request) === null || _c === void 0 ? void 0 : _c.head) === null || _d === void 0 ? void 0 : _d.sha;
-    if (typeof ref !== 'string' || !ref.startsWith('releases/')) {
-        throw new types_1.SkipActionError('Not a branch on releases/*');
-    }
-    if (((_e = payload.pull_request) === null || _e === void 0 ? void 0 : _e.merged) !== true) {
-        throw new types_1.SkipActionError('Not a merged pull request');
-    }
-    const { title, body } = payload.pull_request;
-    if (typeof title !== 'string' || !title.startsWith('[release] ')) {
-        throw new types_1.SkipActionError('[release] not present in title');
-    }
-    if (!payload.repository) {
-        throw new Error('Unable to load repository info');
-    }
-    if (typeof sha !== 'string') {
-        throw new Error('Unable to load commit SHA');
-    }
-    const privateRepo = payload.repository.name;
-    const publicRepo = PRIV_TO_PUB_REPO_MAP[privateRepo];
-    if (!publicRepo) {
-        throw new Error(`Unable to get public repo for ${privateRepo}`);
-    }
-    const parts = title.split(' ').slice(1);
-    const version = parts[0];
-    return {
-        version,
-        title: parts.join(' '),
-        body: body !== null && body !== void 0 ? body : '',
-        publicRepo,
-        privateRepo,
-        sha
-    };
-}
-exports.validateAndExtractArgsFromPayload = validateAndExtractArgsFromPayload;
-function createGitRepoUrl(token, repo) {
-    return `https://oauth2:${token}@github.com/statsig-io/${repo}.git`;
-}
-exports.createGitRepoUrl = createGitRepoUrl;
-
-
-/***/ }),
-
 /***/ 3109:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -99,40 +41,21 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(2186));
 const github = __importStar(__nccwpck_require__(5438));
-const simple_git_1 = __nccwpck_require__(9103);
-const helpers_1 = __nccwpck_require__(5008);
+const release_1 = __nccwpck_require__(878);
 const types_1 = __nccwpck_require__(8164);
+const prepare_1 = __nccwpck_require__(8837);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const payload = github.context.payload;
             core.debug(`Payload: ${JSON.stringify(payload)}`);
-            const args = (0, helpers_1.validateAndExtractArgsFromPayload)(payload);
-            core.debug(`Extracted args: ${JSON.stringify(args)}`);
-            const { title, body, version, privateRepo, publicRepo, sha } = args;
-            const token = core.getInput('gh-token');
-            const git = (0, simple_git_1.simpleGit)();
-            const dir = process.cwd() + '/private-sdk';
-            yield git
-                .clone((0, helpers_1.createGitRepoUrl)(token, privateRepo), dir)
-                .then(() => git
-                .cwd(dir)
-                .addConfig('user.name', 'statsig-kong[bot]')
-                .addConfig('user.email', 'statsig-kong[bot]@users.noreply.github.com'))
-                .then(() => git.checkout(sha))
-                .then(() => git.addAnnotatedTag(version, title))
-                .then(() => git.addRemote('public', (0, helpers_1.createGitRepoUrl)(token, publicRepo)))
-                .then(() => git.push('public', 'main', ['--follow-tags']));
-            const octokit = github.getOctokit(token);
-            const response = yield octokit.rest.repos.createRelease({
-                owner: 'statsig-io',
-                repo: publicRepo,
-                tag_name: version,
-                body,
-                name: title,
-                generate_release_notes: true
-            });
-            console.log(`Released: ${response}`);
+            switch (payload.action) {
+                case 'opened':
+                case 'reopened':
+                    return (0, prepare_1.prepare)(payload);
+                case 'closed':
+                    return (0, release_1.release)(payload);
+            }
         }
         catch (error) {
             if (error instanceof types_1.SkipActionError) {
@@ -147,6 +70,154 @@ function run() {
     });
 }
 run();
+
+
+/***/ }),
+
+/***/ 8837:
+/***/ (function(__unused_webpack_module, exports) {
+
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.prepare = void 0;
+function prepare(payload) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log(`Got Payload: ${JSON.stringify(payload)}`);
+    });
+}
+exports.prepare = prepare;
+
+
+/***/ }),
+
+/***/ 878:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.release = void 0;
+const core = __importStar(__nccwpck_require__(2186));
+const github = __importStar(__nccwpck_require__(5438));
+const simple_git_1 = __nccwpck_require__(9103);
+const types_1 = __nccwpck_require__(8164);
+const PRIV_TO_PUB_REPO_MAP = {
+    'private-js-client-sdk': 'js-client',
+    'private-rust-sdk': 'rust-sdk',
+    'test-sdk-repo-private': 'test-sdk-repo-public'
+};
+function release(payload) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const args = validateAndExtractArgsFromPayload(payload);
+        core.debug(`Extracted args: ${JSON.stringify(args)}`);
+        const { title, body, version, privateRepo, publicRepo, sha } = args;
+        const token = core.getInput('gh-token');
+        const git = (0, simple_git_1.simpleGit)();
+        const dir = process.cwd() + '/private-sdk';
+        yield git
+            .clone(createGitRepoUrl(token, privateRepo), dir)
+            .then(() => git
+            .cwd(dir)
+            .addConfig('user.name', 'statsig-kong[bot]')
+            .addConfig('user.email', 'statsig-kong[bot]@users.noreply.github.com'))
+            .then(() => git.checkout(sha))
+            .then(() => git.addAnnotatedTag(version, title))
+            .then(() => git.addRemote('public', createGitRepoUrl(token, publicRepo)))
+            .then(() => git.push('public', 'main', ['--follow-tags']));
+        const octokit = github.getOctokit(token);
+        const response = yield octokit.rest.repos.createRelease({
+            owner: 'statsig-io',
+            repo: publicRepo,
+            tag_name: version,
+            body,
+            name: title,
+            generate_release_notes: true
+        });
+        console.log(`Released: ${response}`);
+    });
+}
+exports.release = release;
+function validateAndExtractArgsFromPayload(payload) {
+    var _a, _b, _c, _d, _e;
+    const ref = (_b = (_a = payload.pull_request) === null || _a === void 0 ? void 0 : _a.head) === null || _b === void 0 ? void 0 : _b.ref;
+    const sha = (_d = (_c = payload.pull_request) === null || _c === void 0 ? void 0 : _c.head) === null || _d === void 0 ? void 0 : _d.sha;
+    if (typeof ref !== 'string' || !ref.startsWith('releases/')) {
+        throw new types_1.SkipActionError('Not a branch on releases/*');
+    }
+    if (((_e = payload.pull_request) === null || _e === void 0 ? void 0 : _e.merged) !== true) {
+        throw new types_1.SkipActionError('Not a merged pull request');
+    }
+    const { title, body } = payload.pull_request;
+    if (typeof title !== 'string' || !title.startsWith('[release] ')) {
+        throw new types_1.SkipActionError('[release] not present in title');
+    }
+    if (!payload.repository) {
+        throw new Error('Unable to load repository info');
+    }
+    if (typeof sha !== 'string') {
+        throw new Error('Unable to load commit SHA');
+    }
+    const privateRepo = payload.repository.name;
+    const publicRepo = PRIV_TO_PUB_REPO_MAP[privateRepo];
+    if (!publicRepo) {
+        throw new Error(`Unable to get public repo for ${privateRepo}`);
+    }
+    const parts = title.split(' ').slice(1);
+    const version = parts[0];
+    return {
+        version,
+        title: parts.join(' '),
+        body: body !== null && body !== void 0 ? body : '',
+        publicRepo,
+        privateRepo,
+        sha
+    };
+}
+function createGitRepoUrl(token, repo) {
+    return `https://oauth2:${token}@github.com/statsig-io/${repo}.git`;
+}
 
 
 /***/ }),
