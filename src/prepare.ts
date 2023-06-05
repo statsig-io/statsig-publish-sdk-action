2dedf6af6b6ca3c94eafd9ec5e8bd2883e1531a9
Daniel Loomb <daniel@statsig.com>
2023-06-05 16:20:33 -0700

    Update prepare.ts

--- src/prepare.ts
+++ src/prepare.ts
@@ -58,6 +58,7 @@ export async function prepare(payload: WebhookPayload) {
   switch (payload.repository?.name) {
     case 'test-sdk-repo-private':
     case 'private-js-client-sdk':
+    case 'private-node-js-server-sdk':
       return runNpmInstall(payload);
 
     default:
