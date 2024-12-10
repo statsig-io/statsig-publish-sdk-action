62ad39eaf4381003f82044df2c17e223c1ac36a7
Daniel Loomb <daniel@statsig.com>
2024-12-09 17:21:14 -0800

    chore: add support for jsmono sync-version

--- core/dist/index.js
+++ core/dist/index.js
@@ -362,6 +362,42 @@ function runNpmInstall(payload) {
         });
     });
 }
+function runJsMonorepoVersionSync(payload) {
+    var _a, _b, _c;
+    return __awaiter(this, void 0, void 0, function* () {
+        const repo = (_a = payload.repository) === null || _a === void 0 ? void 0 : _a.name;
+        const branch = (_c = (_b = payload.pull_request) === null || _b === void 0 ? void 0 : _b.head) === null || _c === void 0 ? void 0 : _c.ref;
+        if (!repo || !branch) {
+            throw new Error('Missing required information');
+        }
+        core.debug(`Running pnpm sync-version: ${repo} ${branch}`);
+        const token = yield kong_octokit_1.default.token();
+        const git = (0, simple_git_1.simpleGit)();
+        const dir = process.cwd() + '/private-sdk';
+        yield git
+            .clone((0, helpers_1.createGitRepoUrl)(token, repo), dir)
+            .then(() => git
+            .cwd(dir)
+            .addConfig('user.name', 'statsig-kong[bot]')
+            .addConfig('user.email', 'statsig-kong[bot]@users.noreply.github.com'))
+            .then(() => git.checkout(branch));
+        (0, child_process_1.execSync)('pnpm install', { cwd: dir });
+        (0, child_process_1.execSync)('pnpm sync-version', { cwd: dir, stdio: 'inherit' });
+        yield git.status().then(status => {
+            if (status.isClean()) {
+                return;
+            }
+            const supported = ['package-lock.json', 'src/SDKVersion.ts'];
+            const files = status.files
+                .filter(file => supported.includes(file.path))
+                .map(file => file.path);
+            return git
+                .add(files)
+                .then(() => git.commit(`Bot: Updated File/s [${files.join(', ')}]`))
+                .then(() => git.push('origin', branch));
+        });
+    });
+}
 function prepareForRelease(payload) {
     var _a, _b, _c, _d, _e, _f, _g, _h;
     return __awaiter(this, void 0, void 0, function* () {
@@ -390,6 +426,8 @@ function prepareForRelease(payload) {
             case 'private-react-sdk':
             case 'private-react-native':
                 return runNpmInstall(payload);
+            case 'private-js-client-monorepo':
+                return runJsMonorepoVersionSync(payload);
             case 'private-python-sdk':
             case 'private-go-sdk':
                 throw new types_1.SkipActionError(`Prepare not neccessary for repository: ${(_f = (_e = payload.repository) === null || _e === void 0 ? void 0 : _e.name) !== null && _f !== void 0 ? _f : null}`);
--- core/src/prepare.ts
+++ core/src/prepare.ts
@@ -49,6 +49,50 @@ async function runNpmInstall(payload: WebhookPayload) {
   });
 }
 
+async function runJsMonorepoVersionSync(payload: WebhookPayload) {
+  const repo = payload.repository?.name;
+  const branch = payload.pull_request?.head?.ref;
+
+  if (!repo || !branch) {
+    throw new Error('Missing required information');
+  }
+
+  core.debug(`Running pnpm sync-version: ${repo} ${branch}`);
+
+  const token = await KongOctokit.token();
+  const git: SimpleGit = simpleGit();
+  const dir = process.cwd() + '/private-sdk';
+
+  await git
+    .clone(createGitRepoUrl(token, repo), dir)
+    .then(() =>
+      git
+        .cwd(dir)
+        .addConfig('user.name', 'statsig-kong[bot]')
+        .addConfig('user.email', 'statsig-kong[bot]@users.noreply.github.com')
+    )
+    .then(() => git.checkout(branch));
+
+  execSync('pnpm install', { cwd: dir });
+  execSync('pnpm sync-version', { cwd: dir, stdio: 'inherit' });
+
+  await git.status().then(status => {
+    if (status.isClean()) {
+      return;
+    }
+
+    const supported = ['package-lock.json', 'src/SDKVersion.ts'];
+    const files = status.files
+      .filter(file => supported.includes(file.path))
+      .map(file => file.path);
+
+    return git
+      .add(files)
+      .then(() => git.commit(`Bot: Updated File/s [${files.join(', ')}]`))
+      .then(() => git.push('origin', branch));
+  });
+}
+
 export async function prepareForRelease(payload: WebhookPayload) {
   if (!payload.repository) {
     throw new Error('Failed to load repository information');
@@ -80,6 +124,9 @@ export async function prepareForRelease(payload: WebhookPayload) {
     case 'private-react-sdk':
     case 'private-react-native':
       return runNpmInstall(payload);
+
+    case 'private-js-client-monorepo':
+      return runJsMonorepoVersionSync(payload);
 
     case 'private-python-sdk':
     case 'private-go-sdk':
