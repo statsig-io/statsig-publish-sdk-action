aac868a60d2076f31490124972cded13e1373711
“Kat <kat@statsig.com>
2025-02-26 16:03:43 -0800

    map js-mono repos

--- core/src/release.ts
+++ core/src/release.ts
@@ -38,6 +38,7 @@ const PRIV_TO_PUB_REPO_MAP: Record<string, string> = {
   'private-statsig-server-core': 'statsig-server-core',
   'private-swift-on-device-evaluations-sdk': 'swift-on-device-evaluations-sdk',
   'private-unity-sdk': 'unity-sdk',
+  'private-js-client-monorepo': 'js-client-monorepo',
   'test-sdk-repo-private': 'test-sdk-repo-public'
 };
 
