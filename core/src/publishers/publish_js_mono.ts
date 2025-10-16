e61cad16b67a3d47aaef031e5d1e31a6854e3f66
Andre Terron <andre@statsig.com>
2025-10-15 17:27:35 -0700

    Log command results as strings instead of buffers (#38)
    Command outputs are being logged as buffers instead of strings, making
    it harder to debug issues.

--- core/src/publishers/publish_js_mono.ts
+++ core/src/publishers/publish_js_mono.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core';
 
 import { PublishActionArgs } from './action_args';
-import { execSync } from 'child_process';
+import { execSync, ExecSyncOptionsWithStringEncoding } from 'child_process';
 
 export default async function publishJSMono(args: PublishActionArgs) {
   const NPM_TOKEN = core.getInput('npm-token') ?? '';
@@ -12,11 +12,12 @@ export default async function publishJSMono(args: PublishActionArgs) {
   const commands = [
     'pnpm install',
     `echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc`,
-    `pnpm exec nx run statsig:publish-all --verbose`,
+    `pnpm exec nx run statsig:publish-all --verbose`
   ];
 
-  const opts = {
-    cwd: args.workingDir
+  const opts: ExecSyncOptionsWithStringEncoding = {
+    cwd: args.workingDir,
+    encoding: 'utf8'
   };
 
   for await (const command of commands) {
